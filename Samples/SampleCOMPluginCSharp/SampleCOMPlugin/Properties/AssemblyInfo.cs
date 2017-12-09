87e9c6d705b8bb417dcab41469177d89582c5ba1
Charles Lechasseur <shiftingbeard@gmx.com>
2017-12-08 22:45:33 -0500

    Move to version 15.0

--- Samples/SampleCOMPluginCSharp/SampleCOMPlugin/Properties/AssemblyInfo.cs
+++ Samples/SampleCOMPluginCSharp/SampleCOMPlugin/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("14.0.1.0")]
-[assembly: AssemblyFileVersion("14.0.1.0")]
+[assembly: AssemblyVersion("15.0.0.0")]
+[assembly: AssemblyFileVersion("15.0.0.0")]
