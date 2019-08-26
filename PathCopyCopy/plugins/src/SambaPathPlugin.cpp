d617b48258c2bcaf979adf6b8cffcd1601a20b6e
Charles Lechasseur <shiftingbeard@gmx.com>
2019-08-25 20:40:42 -0400

    Fix copyright in some files

--- PathCopyCopy/plugins/src/SambaPathPlugin.cpp
+++ PathCopyCopy/plugins/src/SambaPathPlugin.cpp
@@ -1,5 +1,5 @@
 // SambaPathPlugin.cpp
-// (c) 2018, Charles Lechasseur
+// (c) 2018-2019, Charles Lechasseur
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
