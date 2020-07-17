86daedd3560684d022142847f38ce3fc464f3fe4
Charles Lechasseur <shiftingbeard@gmx.com>
2020-07-16 21:42:58 -0400

    #107 : Localized software update form

--- PathCopyCopySettings/UI/Forms/SoftwareUpdateForm.Designer.cs
+++ PathCopyCopySettings/UI/Forms/SoftwareUpdateForm.Designer.cs
@@ -28,6 +28,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SoftwareUpdateForm));
             this.SoftwareUpdateAvailLbl = new System.Windows.Forms.Label();
             this.SoftwareUpdateTitleLbl = new System.Windows.Forms.Label();
             this.SoftwareUpdateUrlLinkLbl = new System.Windows.Forms.LinkLabel();
@@ -40,91 +41,62 @@
             // 
             // SoftwareUpdateAvailLbl
             // 
-            this.SoftwareUpdateAvailLbl.AutoSize = true;
-            this.SoftwareUpdateAvailLbl.Location = new System.Drawing.Point(12, 9);
+            resources.ApplyResources(this.SoftwareUpdateAvailLbl, "SoftwareUpdateAvailLbl");
             this.SoftwareUpdateAvailLbl.Name = "SoftwareUpdateAvailLbl";
-            this.SoftwareUpdateAvailLbl.Size = new System.Drawing.Size(211, 13);
-            this.SoftwareUpdateAvailLbl.TabIndex = 0;
-            this.SoftwareUpdateAvailLbl.Text = "A new Path Copy Copy version is available:";
+            this.SoftwareUpdatesTooltip.SetToolTip(this.SoftwareUpdateAvailLbl, resources.GetString("SoftwareUpdateAvailLbl.ToolTip"));
             // 
             // SoftwareUpdateTitleLbl
             // 
-            this.SoftwareUpdateTitleLbl.AutoSize = true;
-            this.SoftwareUpdateTitleLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.SoftwareUpdateTitleLbl.Location = new System.Drawing.Point(229, 9);
+            resources.ApplyResources(this.SoftwareUpdateTitleLbl, "SoftwareUpdateTitleLbl");
             this.SoftwareUpdateTitleLbl.Name = "SoftwareUpdateTitleLbl";
-            this.SoftwareUpdateTitleLbl.Size = new System.Drawing.Size(96, 13);
-            this.SoftwareUpdateTitleLbl.TabIndex = 1;
-            this.SoftwareUpdateTitleLbl.Text = "<Update name>";
+            this.SoftwareUpdatesTooltip.SetToolTip(this.SoftwareUpdateTitleLbl, resources.GetString("SoftwareUpdateTitleLbl.ToolTip"));
             // 
             // SoftwareUpdateUrlLinkLbl
             // 
-            this.SoftwareUpdateUrlLinkLbl.AutoSize = true;
-            this.SoftwareUpdateUrlLinkLbl.Location = new System.Drawing.Point(12, 31);
+            resources.ApplyResources(this.SoftwareUpdateUrlLinkLbl, "SoftwareUpdateUrlLinkLbl");
             this.SoftwareUpdateUrlLinkLbl.Name = "SoftwareUpdateUrlLinkLbl";
-            this.SoftwareUpdateUrlLinkLbl.Size = new System.Drawing.Size(291, 13);
-            this.SoftwareUpdateUrlLinkLbl.TabIndex = 2;
             this.SoftwareUpdateUrlLinkLbl.TabStop = true;
-            this.SoftwareUpdateUrlLinkLbl.Text = "Click here to visit the website and download the new update";
+            this.SoftwareUpdatesTooltip.SetToolTip(this.SoftwareUpdateUrlLinkLbl, resources.GetString("SoftwareUpdateUrlLinkLbl.ToolTip"));
             this.SoftwareUpdateUrlLinkLbl.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.SoftwareUpdateUrlLinkLbl_LinkClicked);
             // 
             // ReleaseNotesWebBrowser
             // 
+            resources.ApplyResources(this.ReleaseNotesWebBrowser, "ReleaseNotesWebBrowser");
             this.ReleaseNotesWebBrowser.AllowNavigation = false;
             this.ReleaseNotesWebBrowser.AllowWebBrowserDrop = false;
-            this.ReleaseNotesWebBrowser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
             this.ReleaseNotesWebBrowser.IsWebBrowserContextMenuEnabled = false;
-            this.ReleaseNotesWebBrowser.Location = new System.Drawing.Point(96, 56);
-            this.ReleaseNotesWebBrowser.MinimumSize = new System.Drawing.Size(20, 20);
             this.ReleaseNotesWebBrowser.Name = "ReleaseNotesWebBrowser";
             this.ReleaseNotesWebBrowser.ScriptErrorsSuppressed = true;
-            this.ReleaseNotesWebBrowser.Size = new System.Drawing.Size(374, 283);
-            this.ReleaseNotesWebBrowser.TabIndex = 4;
+            this.SoftwareUpdatesTooltip.SetToolTip(this.ReleaseNotesWebBrowser, resources.GetString("ReleaseNotesWebBrowser.ToolTip"));
             this.ReleaseNotesWebBrowser.WebBrowserShortcutsEnabled = false;
             // 
             // ReleaseNotesLbl
             // 
-            this.ReleaseNotesLbl.AutoSize = true;
-            this.ReleaseNotesLbl.Location = new System.Drawing.Point(12, 56);
+            resources.ApplyResources(this.ReleaseNotesLbl, "ReleaseNotesLbl");
             this.ReleaseNotesLbl.Name = "ReleaseNotesLbl";
-            this.ReleaseNotesLbl.Size = new System.Drawing.Size(78, 13);
-            this.ReleaseNotesLbl.TabIndex = 3;
-            this.ReleaseNotesLbl.Text = "Release notes:";
+            this.SoftwareUpdatesTooltip.SetToolTip(this.ReleaseNotesLbl, resources.GetString("ReleaseNotesLbl.ToolTip"));
             // 
             // CloseBtn
             // 
-            this.CloseBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            resources.ApplyResources(this.CloseBtn, "CloseBtn");
             this.CloseBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.CloseBtn.Location = new System.Drawing.Point(395, 345);
             this.CloseBtn.Name = "CloseBtn";
-            this.CloseBtn.Size = new System.Drawing.Size(75, 23);
-            this.CloseBtn.TabIndex = 6;
-            this.CloseBtn.Text = "&Close";
-            this.SoftwareUpdatesTooltip.SetToolTip(this.CloseBtn, "Close the window without applying this update; the update will be offered again a" +
-        "t a later time");
+            this.SoftwareUpdatesTooltip.SetToolTip(this.CloseBtn, resources.GetString("CloseBtn.ToolTip"));
             this.CloseBtn.UseVisualStyleBackColor = true;
             // 
             // IgnoreUpdateBtn
             // 
-            this.IgnoreUpdateBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.IgnoreUpdateBtn.Location = new System.Drawing.Point(273, 345);
+            resources.ApplyResources(this.IgnoreUpdateBtn, "IgnoreUpdateBtn");
             this.IgnoreUpdateBtn.Name = "IgnoreUpdateBtn";
-            this.IgnoreUpdateBtn.Size = new System.Drawing.Size(116, 23);
-            this.IgnoreUpdateBtn.TabIndex = 5;
-            this.IgnoreUpdateBtn.Text = "&Ignore this update";
-            this.SoftwareUpdatesTooltip.SetToolTip(this.IgnoreUpdateBtn, "Do not apply this update; you will only be notified once another update is releas" +
-        "ed");
+            this.SoftwareUpdatesTooltip.SetToolTip(this.IgnoreUpdateBtn, resources.GetString("IgnoreUpdateBtn.ToolTip"));
             this.IgnoreUpdateBtn.UseVisualStyleBackColor = true;
             this.IgnoreUpdateBtn.Click += new System.EventHandler(this.IgnoreUpdateBtn_Click);
             // 
             // SoftwareUpdateForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.CloseBtn;
-            this.ClientSize = new System.Drawing.Size(482, 380);
             this.Controls.Add(this.IgnoreUpdateBtn);
             this.Controls.Add(this.CloseBtn);
             this.Controls.Add(this.ReleaseNotesLbl);
@@ -134,12 +106,10 @@
             this.Controls.Add(this.SoftwareUpdateAvailLbl);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(498, 419);
             this.Name = "SoftwareUpdateForm";
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "Path Copy Copy software update available";
+            this.SoftwareUpdatesTooltip.SetToolTip(this, resources.GetString("$this.ToolTip"));
             this.ResumeLayout(false);
             this.PerformLayout();
 
