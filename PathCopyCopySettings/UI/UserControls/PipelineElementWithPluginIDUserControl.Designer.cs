814d2678223a4134da15fe5467c23638bf9373c2
Charles Lechasseur <shiftingbeard@gmx.com>
2020-07-18 23:30:46 -0400

    #107 : Localize Plugin UI pipeline element user control

--- PathCopyCopySettings/UI/UserControls/PipelineElementWithPluginIDUserControl.Designer.cs
+++ PathCopyCopySettings/UI/UserControls/PipelineElementWithPluginIDUserControl.Designer.cs
@@ -28,28 +28,26 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PipelineElementWithPluginIDUserControl));
             this.BasePluginLst = new System.Windows.Forms.ListBox();
             this.BasePluginToolTip = new System.Windows.Forms.ToolTip(this.components);
             this.SuspendLayout();
             // 
             // BasePluginLst
             // 
-            this.BasePluginLst.Dock = System.Windows.Forms.DockStyle.Fill;
+            resources.ApplyResources(this.BasePluginLst, "BasePluginLst");
             this.BasePluginLst.FormattingEnabled = true;
-            this.BasePluginLst.Location = new System.Drawing.Point(0, 0);
             this.BasePluginLst.Name = "BasePluginLst";
-            this.BasePluginLst.Size = new System.Drawing.Size(314, 306);
-            this.BasePluginLst.TabIndex = 0;
-            this.BasePluginToolTip.SetToolTip(this.BasePluginLst, "Base command to use to fetch path");
+            this.BasePluginToolTip.SetToolTip(this.BasePluginLst, resources.GetString("BasePluginLst.ToolTip"));
             this.BasePluginLst.SelectedIndexChanged += new System.EventHandler(this.BasePluginLst_SelectedIndexChanged);
             // 
             // PipelineElementWithPluginIDUserControl
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.BasePluginLst);
             this.Name = "PipelineElementWithPluginIDUserControl";
-            this.Size = new System.Drawing.Size(314, 306);
+            this.BasePluginToolTip.SetToolTip(this, resources.GetString("$this.ToolTip"));
             this.ResumeLayout(false);
 
         }
