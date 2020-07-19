ac063eb215088c7e50884e047185f8de37016859
Charles Lechasseur <shiftingbeard@gmx.com>
2020-07-18 21:10:49 -0400

    #107 : Localize copy N path parts user control

--- PathCopyCopySettings/UI/UserControls/CopyNPathPartsPipelineElementUserControl.Designer.cs
+++ PathCopyCopySettings/UI/UserControls/CopyNPathPartsPipelineElementUserControl.Designer.cs
@@ -27,6 +27,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CopyNPathPartsPipelineElementUserControl));
             this.NumPartsTxt = new System.Windows.Forms.TextBox();
             this.CopyTheNLbl = new System.Windows.Forms.Label();
             this.FirstLastCombo = new System.Windows.Forms.ComboBox();
@@ -35,55 +36,40 @@
             // 
             // NumPartsTxt
             // 
-            this.NumPartsTxt.Location = new System.Drawing.Point(100, 0);
-            this.NumPartsTxt.MaxLength = 4;
+            resources.ApplyResources(this.NumPartsTxt, "NumPartsTxt");
             this.NumPartsTxt.Name = "NumPartsTxt";
-            this.NumPartsTxt.Size = new System.Drawing.Size(38, 20);
-            this.NumPartsTxt.TabIndex = 2;
-            this.NumPartsTxt.Text = "0000";
             this.NumPartsTxt.TextChanged += new System.EventHandler(this.NumPartsTxt_TextChanged);
             this.NumPartsTxt.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.NumPartsTxt_KeyPress);
             // 
             // CopyTheNLbl
             // 
-            this.CopyTheNLbl.AutoSize = true;
-            this.CopyTheNLbl.Location = new System.Drawing.Point(-3, 3);
+            resources.ApplyResources(this.CopyTheNLbl, "CopyTheNLbl");
             this.CopyTheNLbl.Name = "CopyTheNLbl";
-            this.CopyTheNLbl.Size = new System.Drawing.Size(49, 13);
-            this.CopyTheNLbl.TabIndex = 0;
-            this.CopyTheNLbl.Text = "&Copy the";
             // 
             // FirstLastCombo
             // 
+            resources.ApplyResources(this.FirstLastCombo, "FirstLastCombo");
             this.FirstLastCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.FirstLastCombo.FormattingEnabled = true;
             this.FirstLastCombo.Items.AddRange(new object[] {
-            "first",
-            "last"});
-            this.FirstLastCombo.Location = new System.Drawing.Point(52, 0);
+            resources.GetString("FirstLastCombo.Items"),
+            resources.GetString("FirstLastCombo.Items1")});
             this.FirstLastCombo.Name = "FirstLastCombo";
-            this.FirstLastCombo.Size = new System.Drawing.Size(42, 21);
-            this.FirstLastCombo.TabIndex = 1;
             this.FirstLastCombo.SelectedIndexChanged += new System.EventHandler(this.FirstLastCombo_SelectedIndexChanged);
             // 
             // PathPartsLbl
             // 
-            this.PathPartsLbl.AutoSize = true;
-            this.PathPartsLbl.Location = new System.Drawing.Point(144, 3);
+            resources.ApplyResources(this.PathPartsLbl, "PathPartsLbl");
             this.PathPartsLbl.Name = "PathPartsLbl";
-            this.PathPartsLbl.Size = new System.Drawing.Size(90, 13);
-            this.PathPartsLbl.TabIndex = 3;
-            this.PathPartsLbl.Text = "part(s) of the path";
             // 
             // CopyNPathPartsPipelineElementUserControl
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.Controls.Add(this.PathPartsLbl);
             this.Controls.Add(this.FirstLastCombo);
             this.Controls.Add(this.CopyTheNLbl);
             this.Controls.Add(this.NumPartsTxt);
             this.Name = "CopyNPathPartsPipelineElementUserControl";
-            this.Size = new System.Drawing.Size(242, 23);
             this.ResumeLayout(false);
             this.PerformLayout();
 
